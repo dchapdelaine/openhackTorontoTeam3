a50c27d411552d305744855262b4934992cb8e3c
David Chapdelaine <dachapde@microsoft.com>
2018-09-13 13:52:24 -0400

    removed unused settings

--- Settings.cs
+++ Settings.cs
@@ -3,10 +3,6 @@ namespace Company.Function
 {
     public static class Settings
     {
-        public const string EndpointUri = "https://openhackteam3step2.documents.azure.com:443/";
-        public const string PrimaryKey = "TfPXieRJBk9FsNAO80jt4yekyIJvXRL4zcYVdxBgIMS5f4TlRYCTPltFqU9hAjbTPvpWMbx6b3lCbfqnIeJzQA==";
-        public const string DBName = "openhack";
-        public const string CollectionName = "ratings";
         public const string GetProductsUrl = "https://serverlessohproduct.trafficmanager.net/api/GetProducts";
         public const string GetProductUrl = "https://serverlessohproduct.trafficmanager.net/api/GetProduct";
         public const string GetUserUrl = "https://serverlessohuser.trafficmanager.net/api/GetUser";
