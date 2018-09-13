a6c8e0f9f9cde13fdae525ca39019c27ec8e4319
Rowan Sciban <rosciban@microsoft.com>
2018-09-13 14:30:38 -0400

    Get Rating Completed

--- GetRating.cs
+++ GetRating.cs
@@ -7,25 +7,28 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs.Host;
 using Newtonsoft.Json;
 using Microsoft.Extensions.Logging;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using System.Collections.Generic;
 
 namespace Company.Function
 {
     public static class GetRating
     {
         [FunctionName("GetRating")]
-        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequest req, ILogger log)
+        public static IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)]HttpRequest req,
+             [CosmosDB(
+                databaseName: "openhack",
+                collectionName: "ratings",
+                ConnectionStringSetting = "CosmosDBConnection", 
+                Id = "{Query.id}")]Rating rating, ILogger log
+            )
+            
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
-
-            string name = req.Query["name"];
-
-            string requestBody = new StreamReader(req.Body).ReadToEnd();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+            return (ActionResult)new OkObjectResult (rating);
         }
     }
 }
